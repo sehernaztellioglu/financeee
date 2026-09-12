6ad8bab12c6ffb0b9d2e60c69ba0c7cc07ed2aed
sehernaztellioglu <sehernaztellioglu@gmail.com>
2026-09-12 21:51:33 +0300

    asd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,15 @@ class Main {
 
 
     static int toplamBalance = 0;
-    static int[] balance = new int[100];
+    static int balance = 0;
     static int[] incomes = new int[100];
     static int[] expenses = new int[100];
 
+
+    static int toplamExpense = 0;
     static int harcamalar;
     static int sayac = -1;
+    static int toplamIncome = 0;
 
     static int counter = 0;
     static int counter2 = 0;
@@ -25,7 +28,7 @@ class Main {
     }
 
     static void addingExpense(int valueOfIncome) {
-        incomes[counter] = valueOfIncome;
+        expenses[counter2] = valueOfIncome;
         counter++;
     }
 
@@ -50,72 +53,70 @@ class Main {
             if (input1 == 1) {
                 System.out.println("Add income:");
                 sayac++;
+                int input2 = Integer.parseInt(scanner.nextLine());
+                addingIncome(input2);
+                System.out.print("income added\n");
+                for(int i = 0; i < 100; i++){
+                    toplamIncome = incomes[i] + toplamIncome;
+
+                }
+
             }
+
+
+
             if (input1 == 2) {
                 System.out.println("Add expenses:");
+                int input2 = Integer.parseInt(scanner.nextLine());
+
+                expenses[counter2] = input2;
+                counter2++;
+                System.out.print("expense added\n");
+
+                for(int i = 0; i < 100; i++){
+                    toplamExpense = expenses[i] + toplamExpense;
+
+                }
+
             }
+
+
+
+
             if (input1 == 3) {
                 System.out.println("Showing balance");
+                int gelir = toplamIncome;
+                int gider = toplamExpense;
+
+                int BALANCE = gelir - gider;
+                balance = BALANCE;
+
+
+
+                System.out.println(balance);
+
             }
+
+
+
             if (input1 == 4) {
                 System.out.println("Showing expenses");
+                for (int eleman : expenses) {
+                    System.out.println(eleman);
+
+
+                }
+
             }
+
+
+
             if (input1 == 5) {
                 System.out.println("EXIT");
                 break;
             }
-            while (true) {
 
-                if (input1 == 3){
-
-                        int gelir = incomes[sayac];
-                        int gider = expenses[sayac];
-
-                        int BALANCE = gelir - gider;
-                        balance[sayac] = BALANCE;
-
-
-
-                        for(int sayi : balance){
-                            toplamBalance += sayi;
-
-                        }
-
-
-
-
-                    System.out.println(toplamBalance);
-                    break;
-
-                }
-                if (input1 == 4) {
-                    for (int eleman : expenses) {
-                        System.out.println(eleman);
-
-
-                    }
-                    break;
-                }
-
-
-                int input2 = Integer.parseInt(scanner.nextLine());
-
-                if (input1 == 1) {
-                    addingIncome(input2);
-                    System.out.print("income added\n");
-                    break;
-                }
-
-                if (input1 == 2) {
-
-                    expenses[counter2] = input2;
-                    counter2++;
-                    System.out.print("expense added\n");
-                    break;
-
-
-                }
             }
         }
     }
-}
+
